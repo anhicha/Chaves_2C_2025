88969b2a932bb95a4d28d55bdec095a7dd528029
Anahi Chaves <natalia.chaves@ingenieria.uner.edu.ar>
2025-09-17 11:58:17 -0300

    se terminan las tareas del ejercicio 1 proyecto 2

--- firmware/projects/guia2_ej1/main/guia2_ej1.c
+++ firmware/projects/guia2_ej1/main/guia2_ej1.c
@@ -31,51 +31,124 @@
 #include "freertos/task.h"
 #include "led.h"
 #include "hc_sr04.h"
+#include "lcditse0803.h"
+#include "switch.h"
+
 /*==================[macros and definitions]=================================*/
+#define CONFIG_BLINK_PERIOD 1000
 
 /*==================[internal data definition]===============================*/
 TaskHandle_t MedirDistancia_task_handle = NULL;
 TaskHandle_t ControlarLed_task_handle = NULL;
 TaskHandle_t Teclas_task_handle = NULL;
-volatile uint16_t distancia_actual=0;
+TaskHandle_t Display_task_handle = NULL;
+
+volatile bool activar_medicion = false; // booleano para activar la medicion
+volatile bool hold = false;             // booleano para mantener el ultimo valor
+volatile uint16_t distancia_actual = 0; // ditancia medida en cm
 
 /*==================[internal functions declaration]=========================*/
-static void MedirDistancia(void *pvParameter){
-	
 
-    while(true){
-        distancia_actual = HcSr04ReadDistanceInCentimeters();
-		printf("Distancia: %d cm\r\n", distancia_actual);
-		vTaskDelay(pdMS_TO_TICKS(500));
-	}
-    }
-
-
-static void ControlarLed(void *pvParameter){
-    while(true){
-        if(distancia_actual < 10 ){
-			LedOff(LED_1);
-			LedOff(LED_2);
-			LedOff(LED_3);
-		}else{
-		}
-		vTaskDelay(pdMS_TO_TICKS(100));
-    
+static void Teclas(void *pvParameter)
+{
+    uint8_t teclas;
+    while (true)
+    {
+        teclas = SwitchesRead();
+        switch (teclas)
+        {
+        case SWITCH_1:
+            activar_medicion = !activar_medicion; // cambio el estado de activar med
+            break;
+        case SWITCH_2:
+            hold = !hold; // cambio el estado de hold
+            break;
+        }
+        vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
     }
 }
 
-static void Teclas(void *pvParameter){
-    while(true){
-      
+static void MedirDistancia(void *pvParameter)
+{
+
+    while (true)
+    {
+        if (activar_medicion)
+        { // si activar med esta en true
+            distancia_actual = HcSr04ReadDistanceInCentimeters();
+            printf("Distancia: %d cm\r\n", distancia_actual);
+        }
+        vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
     }
 }
 
+static void ControlarLed(void *pvParameter)
+{
+    while (true)
+    {
+        if (!activar_medicion)
+        { // si no se activa la medicion
+            LedsOffAll();
+        }
+        else
+        {
+            if (distancia_actual < 10)
+            { // si la distancia es menor a 10 cm
+                LedsOffAll();
+            }
+            else if (distancia_actual >= 10 && distancia_actual < 20)
+            { // si la distancia esta entre 10 y 20 cm
+                LedOn(LED_1);
+                LedOff(LED_2);
+                LedOff(LED_3);
+            }
+            else if (distancia_actual >= 20 && distancia_actual < 30)
+            { // si la distancia esta entre 20 y 30 cm
+                LedOn(LED_2);
+                LedOn(LED_1);
+                LedOff(LED_3);
+            }
+            else if (distancia_actual > 30)
+            { // si la distancia es mayor a 30 cm
+                LedOn(LED_1);
+                LedOn(LED_2);
+                LedOn(LED_3);
+            }
+        }
+        vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+    }
+}
+static void Display(void *pvParameter)
+{
+    while (true)
+    {
+        if (!activar_medicion)
+        { // si no se activa la medicion apago el display
+            LcdItsE0803Off();
+        }
+        else
+        {
+            if (!hold)
+            { // si no se activa hold muestro la distancia actual
+                LcdItsE0803Write(distancia_actual);
+            } // si se activa hold mantengo el ultimo valor en el
+        }
+
+        vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+    }
+}
 /*==================[external functions definition]==========================*/
-void app_main(void){
-	LedsInit();
-    HcSr04Init(GPIO_3, GPIO_2);   
-    xTaskCreate(&MedirDistancia, "LED_1", 512, NULL, 5, &MedirDistancia_task_handle);
-    xTaskCreate(&ControlarLed, "LED_2", 512, NULL, 5, &ControlarLed_task_handle);
-    xTaskCreate(&Teclas, "LED_3", 512, NULL, 5, &Teclas_task_handle);
+void app_main(void)
+{ // Inicializaciones
+    LedsInit();
+    SwitchesInit();
+    LcdItsE0803Init();
+    HcSr04Init(GPIO_3, GPIO_2);
+
+    // crear tareas
+    xTaskCreate(&MedirDistancia, "Medir Distancia", 512, NULL, 5, &MedirDistancia_task_handle);
+    xTaskCreate(&ControlarLed, "Controlar Led", 512, NULL, 5, &ControlarLed_task_handle);
+    xTaskCreate(&Teclas, "Teclas", 512, NULL, 5, &Teclas_task_handle);
+    xTaskCreate(&Display, "Display", 512, NULL, 5, &Display_task_handle);
 }
 /*==================[end of file]============================================*/
